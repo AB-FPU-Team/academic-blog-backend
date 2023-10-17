72af96a5ebefa08cd14cee01f87385418588568c
schoolsupertech <sagemode771@gmail.com>
2023-10-17 17:42:45 +0700

    NEW CUM

--- Academic-Blog-App/Pages/BlogPage/ViewAllBlog.cshtml.cs
+++ Academic-Blog-App/Pages/BlogPage/ViewAllBlog.cshtml.cs
@@ -10,37 +10,37 @@ namespace Academic_Blog_App.Pages.BlogPage
     public class ViewAllBlogModel : PageModel
     {
 
-        private readonly HttpClient _httpClient;
-        private string blogUrl;
+        //private readonly HttpClient _httpClient;
+        //private string blogUrl;
 
-        public ViewAllBlogModel()
-        {
-            _httpClient = new HttpClient();
-            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
-            _httpClient.DefaultRequestHeaders.Accept.Add(contentType);
-            blogUrl = "https://localhost:5047/api/Blogs";
+        public ViewAllBlogModel() {
+            //_httpClient = new HttpClient();
+            //var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            //_httpClient.DefaultRequestHeaders.Accept.Add(contentType);
+            //blogUrl = "https://localhost:5047/api/Blogs";
         }
 
-        [BindProperty]
-        public List<Blog> Blogs { get; set; } = default!;
-        public async Task<IActionResult> OnGetAsync()
-        {
-            HttpResponseMessage response = await _httpClient.GetAsync(blogUrl);
 
-            if (response.IsSuccessStatusCode)
-            {
-                string content = await response.Content.ReadAsStringAsync();
-                var options = new JsonSerializerOptions
-                {
-                    PropertyNameCaseInsensitive = true
-                };
-                Blogs = JsonSerializer.Deserialize<List<Blog>>(content, options)!;
-            }
-            else
-            {
-                ViewData["Error"]  = response.ToString();
-            }
-            return Page();
-        }
+        //[BindProperty]
+        //public List<Blog> Blogs { get; set; } = default!;
+        //public async Task<IActionResult> OnGetAsync()
+        //{
+        //    HttpResponseMessage response = await _httpClient.GetAsync(blogUrl);
+
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        string content = await response.Content.ReadAsStringAsync();
+        //        var options = new JsonSerializerOptions
+        //        {
+        //            PropertyNameCaseInsensitive = true
+        //        };
+        //        Blogs = JsonSerializer.Deserialize<List<Blog>>(content, options)!;
+        //    }
+        //    else
+        //    {
+        //        ViewData["Error"]  = response.ToString();
+        //    }
+        //    return Page();
+        //}
     }
 }
